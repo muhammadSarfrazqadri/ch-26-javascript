069e87998ebf810854a3c20dc00ed008d810c390
Muhammad Sarfraz <msowner.best@gmail.com>
2024-11-24 11:29:25 +0500

    add ch 27 assignment

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 var input = prompt("Write an positive Decimal Number !")
 
-console.log("Number is : " + input)
-console.log("Round off value is : " + Math.round(input))
-console.log("Floor value is : " + Math.floor(input))
-console.log("Ceil value is : " + Math.ceil(input))
+
+if(input <= 0){
+    console.log("Number is : " + input)
+    console.log("Round off value is : " + Math.round(input))
+    console.log("Floor value is : " + Math.floor(input))
+    console.log("Ceil value is : " + Math.ceil(input))
+}else{
+    console.log("Number is : " + input)
+    console.log("Round off value is : " + Math.round(input))
+    console.log("Floor value is : " + Math.floor(input))
+    console.log("Ceil value is : " + Math.ceil(input))
+}
