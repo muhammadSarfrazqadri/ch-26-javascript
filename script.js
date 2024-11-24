83e9af12aa8b7d8238b0b988829d3a70227b2e42
Muhammad Sarfraz <msowner.best@gmail.com>
2024-11-24 11:13:43 +0500

    made a new code

---
+++ script.js
@@ -0,0 +1,6 @@
+var input = prompt("Write an positive Decimal Number !")
+
+console.log("Number is : " + input)
+console.log("Round off value is : " + Math.round(input))
+console.log("Floor value is : " + Math.floor(input))
+console.log("Ceil value is : " + Math.ceil(input))
